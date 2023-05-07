157fe1619e445ce8d1e63c45250bab6814a8dbaa
AhnchiYoon <ahnroedeer@gmail.com>
2023-05-08 02:08:11 +0900

    fix

--- Astronaut/urls.py
+++ Astronaut/urls.py
@@ -19,5 +19,3 @@ urlpatterns = [
 # 미디어 폴더에 접근해서 파일들을 조회할 수 있도록 하는 코드입니다.
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns = [path('', views.index, name='index'), ]
