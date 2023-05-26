27acf6a174a68e732f371ad2f81c069b47e6cd26
AhnchiYoon <ahnroedeer@gmail.com>
2023-05-26 22:21:38 +0900

    backup

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from content.models import Feed
+from content.models import Feed, Reply
 
 # Register your models here.
 admin.site.register(Feed)
+admin.site.register(Reply)
