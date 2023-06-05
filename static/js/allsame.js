fe8937f9bc0ea0fab2dc5c91915f310307230300
AhnchiYoon <ahnroedeer@gmail.com>
2023-06-05 21:07:32 +0900

    주석 마무리

--- static/js/allsame.js
+++ static/js/allsame.js
@@ -1,70 +1,77 @@
-// 정유진: 모달 바깥부분을 클릭하면 안 보에게 된다.
+// 자동완성 모달창과 검색창의 아이디 값을 가져온다.
 let auto_modal = document.getElementById('auto_modal').getAttribute("id");
 let search_box = document.getElementById('search_box').getAttribute("id");
 
+// 모달 바깥부분을 클릭하면 안 보에게 된다.
 window.addEventListener('mouseup', function (event) {
-    // 정유진: 모달창 사라지는 기준.
-    if ((event.target.getAttribute("id") == null)
-        || !((event.target.getAttribute("id").includes(auto_modal))
-            || (event.target.getAttribute("id").includes(search_box)))) {
+    // 클릭한 곳의 id 값이 널이거나 auto_modal이 포함되어 있지 않거나 search_box이 포함되어 있지 않으면 창을 안 보이게 설정
+    if ((event.target.getAttribute("id") == null) || !((event.target.getAttribute("id").includes(auto_modal)) || (event.target.getAttribute("id").includes(search_box)))) {
         $('#auto_modal').css({
             display: 'none'
         });
     }
 });
 
-// 정유진: 반응형 적용
+// 검색 자동완성 모달창 반응형 적용, 브라우저 창이 줄어들게 될 경우 아래 코드를 실행
 window.addEventListener('resize', function () {
+    // search_box라는 id를 가진 요소를 가져옴
     let search_box = document.getElementById('search_box');
-    // 정유진: 태그 안의 요소들을 가져온다
+    //  해당 요소의 위치 값 구함
     let rect = search_box.getBoundingClientRect();
-    // 정유진: left 요소의 값을 가져온다
+    // 요소 값들 중에서 left 요소의 값을 구함
     let left_value = rect.left;
-    // 정유진: 검색창이 없을 경우
+    // 검색창이 없을 경우 (left_value가 0이됨)
     if (left_value == 0) {
         $('#auto_modal').css({
             display: 'none'
         });
-    } else { // 정유진: 검색창이 있을 경우
+    } else { // 검색창이 있을 경우 줄어드는 크기에 맞춰서 이동시킴
         $('#auto_modal').css({
             left: left_value + 'px'
         });
     }
 });
 
-// 정유진: 검색창을 클릭하면 모달창이 뜬다
+// 검색창을 클릭하면 자동완성 모달창이 뜬다(이미 창이 줄었을 경우 그때 검색창을 클릭하게 된다면 해당 위치에 보여줘야 함)
 $('#search_box').mousedown(function () {
+    // search_box라는 id를 가진 요소를 가져옴
     let element = document.getElementById('search_box');
+    //  해당 요소의 위치 값 구함
     let rect = element.getBoundingClientRect();
+    // 요소 값들 중에서 left 요소의 값을 구함
     let left_value = rect.left;
-    // 정유진: 모달창 위치를 정하고 보이게 한다
+    // 이동된 검색창 위치를 계산해서 검색창 바로 밑에 자동완성 창을 띄움
     $('#auto_modal').css({
         display: 'flex',
         left: left_value + 'px'
     });
-    // 정유진: 자동완성에 필요한 검색창의 값을 keyup()으로 가져온다.
+    // 자동완성에 필요한 검색창의 값을 keyup 이벤트로 가져온다. TODO 시간차 텀
     $('#search_box').keyup(function () {
         let search_box_value = document.getElementById('search_box').value;
-        // 정유진: 해시태그 검색 시 #만 있을 경우 검색이 안 된다.
-        if (search_box_value != "" & search_box_value !='#') {
-            // 정유진: 가져온 값을 서버로 보낸다.
+        // 해시태그 검색 시 #만 있거나 아무것도 없으면 검색 안됨.
+        if (search_box_value != "" & search_box_value != '#') {
+            // 가져온 값을 서버로 보낸다.
             $.ajax({
                 url: "/content/autocomplete",
                 data: {
                     search_box_value: search_box_value
                 },
                 method: "GET",
+                // data에는 자동 완성에 필요한 검색어로 필터링된 유저의 객체 리스트와, 검색어로 필터링된 해시태그 리스트를 합친 prioritize_list 리스트가 존재
                 success: function (data) {
                     console.log("성공");
+                    // 키 입력이 검색 키워드 변경으로 인해 새로운 자동완성 모달창 다시 만들어줘야 하기 위해서 모달창을 비움
                     $("#auto_modal_list").html('');
+                    // prioritize_list 리스트에 있는 요소 수 만큼 반복
                     for (let i = 0; i < data['prioritize_list'].length; i++) {
-                        // 정유진: 사용자 검색 시
+                        // 사용자 검색 시(prioritize_list 리스트에 content의 정보가 없을 경우 -> 유저 검색)
                         if (data['prioritize_list'][i]['content'] == undefined) {
-                            // 정유진: 이미지는 경로가 따로 있어야 한다.
+                            // prioritize_list 리스트에서 검색 키워드에 필터링된 유저 정보를 꺼냄 (이미지는 경로가 따로 있어야 한다.)
                             let user_profile_image = "/media/" + data['prioritize_list'][i].profile_image;
                             let user_nickname = data['prioritize_list'][i].nickname;
                             let user_name = data['prioritize_list'][i].name;
 
+                            // 자동완성 모달창에 자동완성 목록을 만드는 과정
                             $("#auto_modal_list").append('<div id="auto_modal_object_' + i + '" class="movetoprofile auto_modal_object"></div>');
 
                             $("#auto_modal_object_" + i).append('<img id="' + user_nickname + '" class="box feed_profile_image auto_modal_image" src="' + user_profile_image + '">');
@@ -74,18 +81,16 @@ $('#search_box').mousedown(function () {
                             $("#auto_modal_nickname_name_" + i).append('<div id="' + user_nickname + '" class="auto_modal_text_object2">' + user_name + '</div>');
 
                             // 정유진: append에서 id 값을 user_nickname로 하면 $("#" + user_nickname).append가 되지 않아 따로 바꾼다.
+                            // 자동완성 목록에서 닉네임, 프로필 사진, 이름 뿐만 아니라 나머지 빈 영역을 클릭해도 프로필로 이동이 되더야 하기 때문에 id를 유저 닉네임으로 변경 해줘야 함
                             document.getElementById("auto_modal_object_" + i).setAttribute("id", user_nickname);
-
-                            // 화면에서 다른사용자의 프로필 클릭시 해당 사용자의 프로필로 이동
-                            $(".movetoprofile").click(function (event) {
-                                let user_nickname = event.target.id;
-                                location.href = "/content/reprofile?user_nickname=" + user_nickname;
-                            });
                         }
-                        // 정유진: 해시태그 검색 시
+                        // 해시태그 검색 시
                         else {
+                            // prioritize_list 리스트에서 검색 키워드에 필터링된 해시태그 정보를 꺼냄
                             let hashtag_content = data['prioritize_list'][i].content;
                             let hashtag_count = data['prioritize_list'][i].hashtag_count;
+
+                            // 자동완성 모달창에 자동완성 목록을 만드는 과정
                             $("#auto_modal_list").append('<div id="auto_modal_object_' + i + '" class="hashtags auto_modal_object" hashtag_content="' + hashtag_content + '"></div>');
 
                             $("#auto_modal_object_" + i).append('<span class="auto_modal_hashtag_icon material-symbols-outlined" hashtag_content="' + hashtag_content + '">tag</span>');
@@ -96,11 +101,12 @@ $('#search_box').mousedown(function () {
                         }
                     }
 
-                    // 화면에서 다른사용자의 프로필 클릭시 해당 사용자의 프로필로 이동
+                    // 화면에서 다른 사용자의 프로필 클릭시 해당 사용자의 프로필로 이동
                     $(".movetoprofile").click(function (event) {
                         let user_nickname = event.target.id;
                         location.href = "/content/reprofile?user_nickname=" + user_nickname;
                     });
+                    // 해시태그 클릭시 해시태그 검색이 실행 됨
                     $('.hashtags').click(function () {
                         let hashtag_content = $(this).attr('hashtag_content');
                         location.href = "/content/search/?search=%23" + hashtag_content
--- static/js/join.js
+++ static/js/join.js
@@ -1,43 +1,51 @@
-// 정유진: 가입 버튼 비활성화
+// 처음엔 아무 값도 입력되지 않았기 때문에 기본 값은 가입 버튼 비활성화
 document.getElementById("access_button").disabled = true;
-// 정유진: W3C에 나와있는 정규식은 /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-// 정유진: 이메일 정규식 확인. {,}는 최소 수와 최대 수를 나타낸다.
+// W3C에 나와있는 정규식은 /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+// 이메일 정규식 확인. {,}는 최소 수와 최대 수를 나타낸다.
 let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 //최소 8자 최대 24자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자. 특수 문자는 ~!@#$%^&*_+\-.<>/? 만 가능
 let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*_+\-.<>/?])[A-Za-z\d~!@#$%^&*()_+\-=`[~!@#$%^&*_+\-.<>/?]{8,24}$/;
 
-// 정유진: 버튼 활성화 조건. 이메일, 이름, 닉네임, 비밀번호 모두가 유효성에 만족해야 한다. 만족하면 값을 1로 바꾼다.
+// 버튼 활성화 조건 순서대로 [이메일, 이름, 닉네임, 비밀번호] 모두가 유효성에 만족해야 한다. 만족하면 값을 1로 바꾼다.
 let check = [0, 0, 0, 0]
 
-// 정유진: 이메일 주소 유호성 확인. 이메일 양식. 이메일 주소 길이.
+// 이메일 주소 유호성 확인. 이메일 양식. 이메일 주소 길이.
 $('#input_email').keyup(function () {
+    // 이메일 입력란에서 입력된 값을 가져옴
     let email = $('#input_email').val();
+    // 플로팅 라벨
     let emailError = $('#email_error');
+    // TODO? 왜 있어야하는지?
     emailError.css("color", "#ff0000");
 
-    // 정유진: 이메일 유효성 확인. 이메일 양식.
+    // 사용자가 입력한 값이 저장해놓은 규칙과 일치한지 확인
     if (!emailRegex.test(email)) {
+        // 일치하지 않을 경우 플로팅라벨로 알려주고 check에 0을 저장,
         emailError.show().text("올바른 이메일 양식이 아닙니다!")
         check[0] = 0;
-        // 정유진: 공백으로 지우면 다시 검은 글자.
+        // 만약 입력 값이 공백이 될 경우 -> 사용자가 입력한 값을 다시 지우면 플로팅 라벨을 검은색 글자로 변경.
         if (email == "") {
+            // 다시 입력해야 하기 때문에 플로팅라벨을 기본 값으로 설정하고 check에 0을 저장
             emailError.show().text("이메일 주소");
             emailError.css("color", "#000000");
             check[0] = 0;
         }
-    } else {
-        // 정유진: 이메일 주소 길이 확인.
+    } else { // 사용자가 입력한 이메일 형식은 지킨 경우
+        // 사용자가 입력한 이메일 주소 길이 확인.
         if (email.length > 24) {
+            // 이메일이 너무 긴 경우 형식에 어긋남 -> 플로팅 라벨로 알려줌 -> check에 0을 저장
             emailError.show().text("이메일 주소가 너무 깁니다.")
             check[0] = 0;
-        } else {
-            emailError.show().text("이메일");
+        } else { // 모든 조건을 다 지킨 경우
+            // 플로팅 라벨 기본으로 설정, 정상적인 이메일 형식 이므로 check에 1을 저장
+            emailError.show().text("이메일 주소");
             emailError.css("color", "#000000");
             check[0] = 1;
         }
     }
-    // 정유진: 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
+    // 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비밀번호.
     for (let i = 0; i < check.length; i++) {
+        // 만약 check가 모두 1일 경우 가입버튼 활성화, 하나라도 0일 경우 return으로 인해 가입버튼 비활성화
         if (check[i]) {
             document.getElementById("access_button").disabled = false;
         } else {
@@ -46,22 +54,29 @@ $('#input_email').keyup(function () {
         }
     }
 })
-// 정유진: 이름 유호성 확인.
+
+// 이름 유호성 확인.
 $('#input_name').keyup(function () {
+    // 이름 입력란에서 입력된 값을 가져옴
     let name = $('#input_name').val();
+    // 플로팅 라벨
     let nameError = $('#name_error');
 
+    // 사용자가 이름 입력란을 비워놓은 경우
     if (name == "") {
+        // 플로팅 라벨로 사용자 입력란이 비었다고 알림 check에는 0을 저장
         nameError.css("color", "#ff0000");
         nameError.show().text("이름을 넣어주세요");
         check[1] = 0;
-    } else {
-        // 정유진: 이메일 주소 길이 확인.
+    } else { // 사용자가 이름 입력란에 어떤 값을 입력한 경우
+        // 이름 길이를 체크
         if (name.length > 24) {
+            // 너무 이름이 긴 경우 플로팅 라벨로 알림 , check에는 0을 저장
             nameError.css("color", "#ff0000");
             nameError.show().text("이름이 너무 깁니다.")
             check[1] = 0;
-        } else {
+        } else { // 모든 조건을 만족한 경우
+            // 정삭적인 이름이므로 플로팅 라벨을 원래대로 하고, check에 1을 저장
             nameError.show().text("이름");
             nameError.css("color", "#000000");
             check[1] = 1;
@@ -69,8 +84,9 @@ $('#input_name').keyup(function () {
 
     }
 
-    // 정유진: 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
+    // 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
     for (let i = 0; i < check.length; i++) {
+        // 만약 check가 모두 1일 경우 가입버튼 활성화, 하나라도 0일 경우 return으로 인해 가입버튼 비활성화
         if (check[i]) {
             document.getElementById("access_button").disabled = false;
         } else {
@@ -79,26 +95,33 @@ $('#input_name').keyup(function () {
         }
     }
 })
-// 정유진: 닉네임 유호성 확인. 공백 포함 안 되게. 수정필요
+// 닉네임 유호성 확인
 $('#input_nickname').keyup(function () {
+    // 닉네임 입력란에서 입력된 값을 가져옴
     let nickname = $('#input_nickname').val();
+    // 플로팅 라벨
     let nicknameError = $('#nickname_error');
+    // 사용자가 닉네임 입력란을 비워놓은 경우
     if (nickname == "") {
+        // 플로팅 라벨로 알림, check에는 0을 저장
         nicknameError.css("color", "#ff0000");
         nicknameError.show().text("닉네임을 넣어주세요");
         check[2] = 0;
-    } else {
-        // 정유진: 닉네임에 공백을 포함하지 못하게 한다.
+    } else { // 사용자가 이름 입력란에 어떤 값을 입력한 경우
+        // 닉네임에 공백을 포함하지 못하게 한다.
         if (nickname.indexOf(' ') !== -1) {
+            // 닉네임에 공백이 포함되었다는걸 플로팅 라벨로 알림, check에는 0을 저장
             nicknameError.css("color", "#ff0000");
             nicknameError.show().text("공백은 넣을 수 없습니다.")
             check[2] = 0;
-        } else {
-            // 정유진: 닉네임 길이 제한
+        } else { // 입력값은 있지만 공백이 없는 경우
+            // 닉네임 길이 제한
             if (nickname.length > 24) {
+                // 너무 닉네임이 긴 경우 플로팅 라벨로 알림 , check에는 0을 저장
                 nicknameError.show().text("닉네임이 너무 깁니다.")
                 check[2] = 0;
-            } else {
+            } else { // 모든 조건을 만족한 경우
+                // 정삭적인 닉네임이므로 플로팅 라벨을 원래대로 하고, check에 1을 저장
                 nicknameError.show().text("닉네임");
                 nicknameError.css("color", "#000000");
                 check[2] = 1;
@@ -106,8 +129,9 @@ $('#input_nickname').keyup(function () {
         }
     }
 
-    // 정유진: 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
+    // 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
     for (let i = 0; i < check.length; i++) {
+        // 만약 check가 모두 1일 경우 가입버튼 활성화, 하나라도 0일 경우 return으로 인해 가입버튼 비활성화
         if (check[i]) {
             document.getElementById("access_button").disabled = false;
         } else {
@@ -116,29 +140,35 @@ $('#input_nickname').keyup(function () {
         }
     }
 })
-// 정유진: 비밀번호 유효성 확인
+// 비밀번호 유효성 확인
 $('#input_password').keyup(function () {
+    // 비밀번호 입력란에서 입력된 값을 가져옴
     let password = $('#input_password').val();
+    // 플로팅 라벨
     let passwordError = $('#password_error');
     passwordError.css("color", "#ff0000");
 
-    // 정유진: 유효성 확인
+    // 사용자가 입력한 값이 저장해놓은 규칙과 일치한지 확인
     if (!passwordRegex.test(password)) {
+        // 일치하지 않을 경우 플로팅라벨로 알려주고 check에 0을 저장,
         passwordError.show().text("8~25자, 문자, 숫자, 특수문자 포함")
         check[3] = 0;
+        // 만약 입력 값이 공백이 될 경우 -> 사용자가 입력한 값을 다시 지우면 플로팅 라벨을 검은색 글자로 변경.
         if (password == "") {
             passwordError.show().text("비밀번호");
             passwordError.css("color", "#000000");
             check[3] = 0;
         }
-    } else {
+    } else { // 사용자가 입력한 비밀번호 형식을 지킨 경우
+        // 플로팅 라벨 기본으로 설정, 정상적인 이메일 형식 이므로 check에 1을 저장
         passwordError.show().text("비밀번호");
         passwordError.css("color", "#000000");
         check[3] = 1;
 
     }
-    // 정유진: 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
+    // 버튼 활성화 조건 확인. 이메일, 이름, 닉네임, 비닐번호.
     for (let i = 0; i < check.length; i++) {
+        // 만약 check가 모두 1일 경우 가입버튼 활성화, 하나라도 0일 경우 return으로 인해 가입버튼 비활성화
         if (check[i]) {
             document.getElementById("access_button").disabled = false;
         } else {
@@ -150,7 +180,6 @@ $('#input_password').keyup(function () {
 
 
 // 가입 버튼 클릭시 이벤트 처리
-// + 입력칸들의 유효성을 확인 후 버튼 활성화
 $('#access_button').click(function () {
     // 각각의 입력 폼에 입력된 내용을 가지고 와서 각각의 변수에 저장
     let email = $('#input_email').val();
@@ -158,7 +187,7 @@ $('#access_button').click(function () {
     let nickname = $('#input_nickname').val();
     let password = $('#input_password').val();
 
-    // 서버로 보내기 위해서 접속할 url : "/user/join"이며 보낼 데이터는 사용자 정보, 방식은 POST (Json 형태)
+    // 서버로 보낼 데이터 (Json 형태)
     $.ajax({
         url: "/user/join",
         data: {
@@ -170,7 +199,7 @@ $('#access_button').click(function () {
         method: "POST",
         success: function (data) {
             console.log("성공");
-            // 정유진: 서버에서 받은 데이터가 있다면 메시지(중복 이메일, 닉네임) 알람. 없다면 로그인 페이지로 이동.
+            // 서버에서 받은 데이터가 있다면 메시지(중복 이메일, 닉네임) 알람. 없다면 로그인 페이지로 이동.
             if (data) {
                 alert(data.message)
             } else {
